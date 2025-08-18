c770fd3bb7270f87977a54b2a7fe48492efefd52
folarin-ogungbemi <ftogungbemi@gmai.com>
2025-08-18 16:49:19 +0200

    first word

--- first_word/first_word.c
+++ first_word/first_word.c
@@ -1,1 +1,29 @@
 #include <unistd.h>
+
+int	ft_isspace(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	if (ac != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+	while (ft_isspace(av[1][i]))
+		i++;
+	while (av[1] && av[1][i] && !ft_isspace(av[1][i]))
+	{
+		write(1, &av[1][i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
